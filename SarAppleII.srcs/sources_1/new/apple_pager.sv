4c03b0bb807c8b6d8fc640dac0e6530b7ee7c961
Shachar Shemesh <shachar@shemesh.biz>
2025-04-21 18:35:04 +0200

    Soft switches (untested)

--- SarAppleII.srcs/sources_1/new/apple_pager.sv
+++ SarAppleII.srcs/sources_1/new/apple_pager.sv
@@ -24,10 +24,22 @@ module apple_pager(
     input clock_i,
 
     input cpu_req_valid_i,
+    output logic cpu_req_ack_o,
     input cpu_req_write_i,
     input [15:0] cpu_req_addr_i,
+    input [7:0] cpu_req_data_i,
 
+    output cpu_rsp_valid_o,
+    output [7:0] cpu_rsp_data_o,
+
+    output logic mem_req_valid_o,
     output logic[31:0] mem_req_addr_o,
+    output mem_req_write_o,
+    output [7:0] mem_req_data_o,
+    input mem_req_ack_i,
+
+    input mem_rsp_valid_i,
+    input [7:0] mem_rsp_data_i,
 
     input ctrl_req_valid_i,
     input ctrl_req_write_i,
@@ -36,7 +48,9 @@ module apple_pager(
     output ctrl_req_ack_o,
 
     output logic ctrl_rsp_valid_o,
-    output logic[31:0] ctrl_rsp_data_o
+    output logic[31:0] ctrl_rsp_data_o,
+
+    output ctrl_intr_o
     );
 
 
@@ -48,6 +62,25 @@ localparam NUM_BANKS = 3;
     *  e000-ffff - ROM/LC
     */
 logic [31:0] mapper[NUM_BANKS][2];
+
+logic io_cmd_pending = 1'b0;
+logic [15:0] io_cmd_addr;
+logic io_cmd_write;
+logic [7:0] io_cmd_write_data;
+logic io_rsp_valid = 1'b0;
+logic [7:0] io_rsp_data;
+
+assign ctrl_intr_o = io_cmd_pending;
+
+assign mem_req_write_o = cpu_req_write_i;
+assign mem_req_data_o = cpu_req_data_i;
+
+assign cpu_rsp_valid_o = !io_cmd_pending && mem_rsp_valid_i || io_rsp_valid;
+assign cpu_rsp_data_o = io_cmd_pending ? io_rsp_data : mem_rsp_data_i;
+
+function logic is_io(logic [15:0] addr);
+    is_io = addr[15:12]==8'hc;
+endfunction
 
 function logic[31:0] translate_addr(logic write, logic [15:0] addr);
     case( addr[15:12] )
@@ -63,7 +96,7 @@ function logic[31:0] translate_addr(logic write, logic [15:0] addr);
         8'h9: translate_addr=mapper[MAIN][write] ^ addr;
         8'ha: translate_addr=mapper[MAIN][write] ^ addr;
         8'hb: translate_addr=mapper[MAIN][write] ^ addr;
-        8'hc: translate_addr=mapper[MAIN][write] ^ addr;
+        8'hc: translate_addr=32'hXXXXXXXX;
         8'hd: translate_addr=mapper[BANK_D][write] ^ addr;
         8'he: translate_addr=mapper[BANKS_E_F][write] ^ addr;
         8'hf: translate_addr=mapper[BANKS_E_F][write] ^ addr;
@@ -71,10 +104,25 @@ function logic[31:0] translate_addr(logic write, logic [15:0] addr);
 endfunction
 
 always_comb begin
-    mem_req_addr_o = 32'hX;
+    cpu_req_ack_o = 1'b0;
 
-    if( cpu_req_valid_i ) begin
+    if( is_io(cpu_req_addr_i) ) begin
+        cpu_req_ack_o = !io_cmd_pending;
+    end else begin
+        cpu_req_ack_o = !io_cmd_pending && mem_req_ack_i;
+    end
+end
+
+always_comb begin
+    mem_req_addr_o = 32'hX;
+    mem_req_valid_o = 1'b0;
+
+    if( cpu_req_valid_i && cpu_req_ack_o ) begin
         mem_req_addr_o = translate_addr(cpu_req_write_i, cpu_req_addr_i);
+
+        if( ! is_io(mem_req_addr_o) ) begin
+            mem_req_valid_o = !io_cmd_pending;
+        end
     end
 end
 
@@ -83,6 +131,8 @@ assign ctrl_req_ack_o = 1'b1;
 always_ff@(posedge clock_i) begin
     ctrl_rsp_valid_o <= ctrl_req_valid_i && !ctrl_req_write_i;
     ctrl_rsp_data_o <= 32'hX;   // Reading the registers is not supported
+
+    io_rsp_valid <= 1'b0;
 
     if( ctrl_req_valid_i ) begin
         if( ctrl_req_write_i ) begin
@@ -93,8 +143,28 @@ always_ff@(posedge clock_i) begin
                 16'h000c:       mapper[MAIN][1]         <= ctrl_req_data_i;
                 16'h0010:       mapper[BANK_D][1]       <= ctrl_req_data_i;
                 16'h0014:       mapper[BANKS_E_F][1]    <= ctrl_req_data_i;
+
+                16'h0080:       begin
+                    io_rsp_data <= ctrl_req_data_i[7:0];
+                    io_rsp_valid <= io_cmd_pending && !io_cmd_write;
+                    io_cmd_pending <= 1'b0;
+                end
+            endcase
+        end else begin
+            // CTRL CPU read request
+            case( ctrl_req_addr_i )
+                16'h0080:       begin
+                    ctrl_rsp_data_o <= { io_cmd_pending, io_cmd_write, 6'b000000, io_cmd_write_data, io_cmd_addr };
+                end
             endcase
         end
+    end
+
+    if( cpu_req_valid_i && cpu_req_ack_o && is_io(cpu_req_addr_i) ) begin
+        io_cmd_pending <= 1'b1;
+        io_cmd_addr <= cpu_req_addr_i;
+        io_cmd_write <= cpu_req_write_i;
+        io_cmd_write_data <= cpu_req_data_i;
     end
 end
 
--- SarAppleII.srcs/sources_1/new/top.sv
+++ SarAppleII.srcs/sources_1/new/top.sv
@@ -601,15 +601,18 @@ wire [7:0] bus8_req_data, bus8_rsp_data;
 wire [15:0] bus8_req_addr;
 wire [31:0] bus8_paged_req_addr;
 
-assign bus8_rsp_valid = cache_port_rsp_valid_n[CACHE_PORT_IDX_6502];
+wire pager_req_valid, pager_req_ack, pager_rsp_valid;
+wire [7:0] pager_rsp_data, pager_req_data;
+
+assign bus8_rsp_valid = pager_rsp_valid;
 
 bus_width_adjust#(.IN_WIDTH(8), .OUT_WIDTH(CACHELINE_BITS), .ADDR_WIDTH(32)) bus8_width_adjuster(
     .clock_i( ctrl_cpu_clock ),
-    .in_cmd_valid_i( bus8_req_valid ),
+    .in_cmd_valid_i( pager_req_valid ),
     .in_cmd_addr_i( bus8_paged_req_addr ),
-    .in_cmd_write_mask_i( bus8_req_write ),
-    .in_cmd_write_data_i( bus8_req_data ),
-    .in_rsp_read_data_o( bus8_rsp_data ),
+    .in_cmd_write_mask_i( pager_req_write ),
+    .in_cmd_write_data_i( pager_req_data ),
+    .in_rsp_read_data_o( pager_rsp_data ),
 
     .out_cmd_ready_i( bus8_req_ack ),
     .out_cmd_write_mask_o( cache_port_cmd_write_mask_s[CACHE_PORT_IDX_6502] ),
@@ -627,8 +630,8 @@ freq_div_bus#() freq_div_6502(
     .slow_cmd_valid_i( bus8_req_valid ),
     .slow_cmd_ready_o( bus8_req_ack ),
 
-    .fast_cmd_valid_o(cache_port_cmd_valid_s[CACHE_PORT_IDX_6502]),
-    .fast_cmd_ready_i(cache_port_cmd_ready_n[CACHE_PORT_IDX_6502])
+    .fast_cmd_valid_o( pager_req_valid ),
+    .fast_cmd_ready_i( pager_req_ack )
     );
 
 sar6502_sync apple_cpu(
@@ -649,21 +652,35 @@ sar6502_sync apple_cpu(
 );
 
 apple_pager pager(
-    .clock_i(ctrl_cpu_clock),
+    .clock_i( ctrl_cpu_clock ),
 
-    .cpu_req_valid_i(bus8_req_valid),
-    .cpu_req_write_i(bus8_req_write),
-    .cpu_req_addr_i(bus8_req_addr),
+    .cpu_req_valid_i( pager_req_valid ),
+    .cpu_req_write_i( bus8_req_write ),
+    .cpu_req_data_i( bus8_req_data ),
+    .cpu_req_addr_i( bus8_req_addr ),
+    .cpu_req_ack_o( pager_req_ack ),
 
-    .mem_req_addr_o(bus8_paged_req_addr),
+    .cpu_rsp_valid_o( pager_rsp_valid ),
+    .cpu_rsp_data_o( bus8_rsp_data ),
 
-    .ctrl_req_valid_i(apple_pager_enable),
-    .ctrl_req_write_i(ctrl_dBus_cmd_payload_wr),
-    .ctrl_req_addr_i(ctrl_dBus_cmd_payload_address[15:0]),
-    .ctrl_req_data_i(ctrl_dBus_cmd_payload_data),
-    .ctrl_req_ack_o(apple_pager_req_ack),
-    .ctrl_rsp_valid_o(apple_pager_rsp_valid),
-    .ctrl_rsp_data_o(apple_pager_rsp_data)
+    .mem_req_valid_o( cache_port_cmd_valid_s[CACHE_PORT_IDX_6502] ),
+    .mem_req_addr_o( bus8_paged_req_addr ),
+    .mem_req_write_o( pager_req_write ),
+    .mem_req_data_o( pager_req_data ),
+    .mem_req_ack_i( cache_port_cmd_ready_n[CACHE_PORT_IDX_6502] ),
+
+    .mem_rsp_valid_i( cache_port_rsp_valid_n[CACHE_PORT_IDX_6502] ),
+    .mem_rsp_data_i( pager_rsp_data ),
+
+    .ctrl_req_valid_i( apple_pager_enable ),
+    .ctrl_req_write_i( ctrl_dBus_cmd_payload_wr ),
+    .ctrl_req_addr_i( ctrl_dBus_cmd_payload_address[15:0] ),
+    .ctrl_req_data_i( ctrl_dBus_cmd_payload_data ),
+    .ctrl_req_ack_o( apple_pager_req_ack ),
+    .ctrl_rsp_valid_o( apple_pager_rsp_valid ),
+    .ctrl_rsp_data_o( apple_pager_rsp_data ),
+
+    .ctrl_intr_o( ctrl_software_interrupt )
 );
 
 assign cache_port_cmd_addr_s[CACHE_PORT_IDX_6502] = bus8_paged_req_addr;
